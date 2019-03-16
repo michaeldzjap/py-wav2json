3377377366d309f994e8465a6c780e5c48f21987
enviz <48526315+enviz@users.noreply.github.com>
2019-03-16 23:57:25 +0530

    Update wav2json.py

--- wav2json.py
+++ wav2json.py
@@ -121,7 +121,8 @@ if __name__ == "__main__":
     SR, data = scipy.io.wavfile.read(args.ifile)
     if(data.ndim==1):                            #if the numpy array is 1D,it converts into a 2D numpy array. 
         data = numpy.reshape(data, (-1, 2))       
-    M, numChannels = data.shape                     # nr. of samples in input
+    else:
+        M, numChannels = data.shape                     # nr. of samples in input
 
     # convert fixed point audio data to floating point range -1. to 1.
     if data.dtype == 'int16':
